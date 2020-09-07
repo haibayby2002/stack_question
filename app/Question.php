3e127fe2266f36fbcf899383672a07e3bb9f0fb5
haibayby2002 <haibayby2002@yahoo.com.vn>
2020-09-07 11:28:42 +0700

    Add relation and mutators

--- app/Question.php
+++ app/Question.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Question extends Model
 {
-    //
+    //fillable
+    protected $fillable = [
+        'title', 'body'
+    ];
+
+    //relationship
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    //mutator
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = trim($value);
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
